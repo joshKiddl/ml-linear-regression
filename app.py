15fe250c6c21a6b0d05d6c75758600fe938e19df
JoshKiddl <joshsparkes6@gmail.com>
2023-07-15 19:12:41 +0100

    Added new dataset

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
